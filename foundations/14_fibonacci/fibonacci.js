44899e1751825cd57101bf7b4ffe2283c255b205
FT Odin <fctanner@pm.me>
2026-01-02 10:39:15 +0100

    solve foundations 14 fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (x) {
+  const y = parseInt(x);
+  if (x < 0) return "OOPS";
+  let n1 = 0;
+  let n2 = 1;
+  let nextTerm;
+  for (let i = 1; i <=y; i++) {
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+  }
+  return n1;
 };
 
 // Do not edit below this line
