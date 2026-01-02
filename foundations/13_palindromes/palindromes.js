5f41dc94eddfd502fc24ca676999832c2ffa829c
FT Odin <fctanner@pm.me>
2026-01-02 10:05:39 +0100

    solve foundations 13 palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+   const sanitized =  x.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+   const reversed = sanitized.split('').reverse().join('');
+   const is_equal = sanitized == reversed;
+   return(is_equal);
 };
 
 // Do not edit below this line
