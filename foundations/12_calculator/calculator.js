95b4fdf1541cf732f6e6d978f6b86dc6d37edf03
FT Odin <fctanner@pm.me>
2026-01-02 09:32:25 +0100

    solve foundations 12 calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (x) {
+  const sum = x.reduce((acc, val) => acc + val, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (x) {
+  const prod = x.reduce((acc, val) => acc * val, 1);
+  return prod;
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  let power = base;
+  for (let i = 1; i < exp; i++) {
+    power = power * base;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  if (x === 0) return 1;
+  let acc = x;
+  for (let i = x - 1; i > 0; i--) {
+    acc = acc * i;
+  }
+  return acc;
 };
 
 // Do not edit below this line
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
